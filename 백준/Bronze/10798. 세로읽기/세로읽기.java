c9d0a8594c92b1f883ee210c48e542a094875cbc
Dabin Bae <109493547+kanchoco@users.noreply.github.com>
2023-06-10 00:25:51 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = new String[5];
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 5; i++) {
+			arr[i] = br.readLine();
+		}
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 5; j++) {
+				if(arr[j].length() > i) {
+					sb.append(arr[j].charAt(i));
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
