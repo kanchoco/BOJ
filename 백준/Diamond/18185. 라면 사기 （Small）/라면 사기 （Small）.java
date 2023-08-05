64b3d4115e7780c77595b52f3d232a33b6686cf9
Dabin Bae <109493547+kanchoco@users.noreply.github.com>
2023-08-05 22:37:41 +0900

    [Diamond V] Title: 라면 사기 (Small), Time: 160 ms, Memory: 15120 KB -BaekjoonHub

---
+++ 백준/Diamond/18185. 라면 사기 （Small）/라면 사기 （Small）.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] A = new int[N+2];
+
+		for (int i = 0; i < N; i++) 
+			A[i] = Integer.parseInt(st.nextToken());
+		
+		int idx = 0;
+		int ans = 0;
+		while (idx < N) {
+			if (A[idx] > 0) {
+				int temp = A[idx];
+				ans += 3*temp;
+				temp = Math.min(temp, A[idx+1]);
+				ans += 2*temp;
+				A[idx+1] -= temp;
+				temp = Math.min(temp, A[idx+2]-Math.min(A[idx+1], A[idx+2]));
+				ans += 2*temp;
+				A[idx+2] -= temp;
+			}
+			idx++;
+		}
+		System.out.println(ans);
+	}
+}
