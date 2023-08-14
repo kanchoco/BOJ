0f7345b55aae65a87f973f73843575b2f66b58ad
Dabin Bae <109493547+kanchoco@users.noreply.github.com>
2023-08-14 20:50:46 +0900

    [Silver I] Title: 1로 만들기 2, Time: 180 ms, Memory: 23728 KB -BaekjoonHub

---
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] d = new int[n+1];	
+		int[] path = new int[n+1];
+		Arrays.fill(d, Integer.MAX_VALUE);
+		
+		d[1] = 0;
+		for (int i = 2; i <= n; i++) {
+
+			if(i%3 == 0) {
+				if(d[i/3] +1 < d[i]) {
+					d[i] = d[i/3] +1;
+					path[i] = i/3;
+				}
+			}
+			if(i%2 == 0) {
+				if(d[i/2] +1 < d[i]) {
+					d[i] = d[i/2] +1;
+					path[i] = i/2;
+				}
+			}
+			
+			if(d[i-1] +1 < d[i]) {
+				d[i] = d[i-1] +1;
+				path[i] = i-1;
+			}
+			
+		}
+		
+		sb.append(d[n]+"\n");
+		
+		while (n > 0) {
+			sb.append(n+" ");
+			n =path[n];
+			
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
